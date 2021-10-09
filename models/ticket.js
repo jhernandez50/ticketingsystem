cc170607aa4c1a5dcdda3ba44b81cd6a43572513
Emanuel Alarcon <emanuellrcn@gmail.com>
2021-10-09 12:38:50 -0500

    Updating routes for Tickets

--- models/ticket.js
+++ models/ticket.js
@@ -1,60 +1,35 @@
-const { Model, DataTypes } = require('sequelize');
+'use strict';
 
-const sequelize = require('../config/connection.js');
-
-class Ticket extends Model {}
-
-Ticket.init(
-  {
-    // Ticket ID
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-  },
-
-   // Describe the request the customer needs
-  ticket_description: {
-      type: DataTypes.STRING,
-      allowNull: false
-  },
-  // Indicate if this is a Incident ticket or Request Ticket
-  ticket_type: {
-    type: DataTypes.STRING,
-    allowNull: false
- },
-  // Write how you solve the Incident or Rquest
-  ticket_resolution: {
-    type: DataTypes.STRING,
-    allowNull: false
- },
-
- customer_id: {
-    type: DataTypes.INTEGER,
-    references: {
-        model: "customer",
-        key: "id"
+module.exports = (sequelize, DataTypes) => {
+    const Ticket = sequelize.define('Ticket', {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        // Customer Name
+        customer_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        // Customer Address
+        customer_address: {
+          type: DataTypes.STRING,
+          allowNull: false
+       },
+      // Customer phone
+        customer_phone: {
+          type: DataTypes.INTEGER,
+          allowNull: false
+       }
+    });
+    Ticket.associate = models => {
+        Ticket.belongsTo(models.Customer, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
     }
-},
-itmember_id: {
-    type: DataTypes.INTEGER,
-    references: {
-        model: "itmember",
-        key: "id"
-    }
-
-},
-
-
-},
- {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'Ticket',
-  }
-);
-
-module.exports = Ticket;
+    return Ticket;
+}
